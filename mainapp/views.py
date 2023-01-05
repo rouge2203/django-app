e5cf87c3dd57c22c4f9233aff1b8fb755291fc49
Alejandro <jruiz60480@ufide.ac.cr>
2023-01-04 22:56:24 -0600

    agregando static files y view nueva con create en db

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Person
 
 def index(request):
+    if request.method == 'POST':
+        person = Person.objects.create(
+            name = request.POST['name'],
+            age = request.POST['age']
+        )
+        person.save()
+        return redirect(index)
+        
     return render(request,'index.html')
 
